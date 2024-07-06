b2b959f2e7375bda866daba090dcf30551b97529
AndreyJustino <andreyjustino2020@gmail.com>
2024-07-06 01:25:43 -0300

    nova funcionalidade: bandeira de paises

---
+++ src/controller/continentes/bandeira.js
@@ -0,0 +1,30 @@
+import express from "express"
+import data from "../../repository/data.js"
+
+const router = express.Router()
+
+router.get('/:nome', async(req, res) => {
+    const nome = req.params.nome.toLocaleUpperCase()
+
+    let index = data.siglas.findIndex((value) => {
+        if(value.nome.toLocaleUpperCase() == nome){
+            return value.sigla
+        }
+    })
+
+    if(index == -1){
+        res.status(404).send("Siglas não encontradas")
+    }
+
+    let sigla = data.siglas[index].sigla
+
+    const url = await fetch(`https://flagsapi.com/${sigla}/shiny/64.png`)
+    const buffe = await url.arrayBuffer()
+
+    res.set('Content-Type', 'image/png');
+    res.set('Content-Disposition', `inline; filename="${sigla}.png"`);
+
+    res.send(Buffer.from(buffe));
+})
+
+export default router
--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import express from "express";
 
+import bandeira from "./controller/continentes/bandeira.js"
 import estado from "./controller/estados/estado.js";
 import estadoId from "./controller/estados/estadoId.js";
 import estadoNome from "./controller/estados/estadoNome.js";
@@ -8,6 +9,7 @@ import continentePais from "./controller/continentes/continentePais.js";
 
 const router = express();
 
+router.use("/bandeira", bandeira)
 router.use("/estado", estado);
 router.use("/estadoId", estadoId);
 router.use("/estadoNome", estadoNome);
