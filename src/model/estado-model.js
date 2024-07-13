a3359878c783745148ffbdcc569d67aef5cf21ba
AndreyJustino <andreyjustino2020@gmail.com>
2024-07-13 11:39:56 -0300

    criando model

---
+++ src/model/estado-model.js
@@ -0,0 +1,9 @@
+export class Estado {
+    constructor(id, nome, sigla, capital, habitantes) {
+      this.id = id;
+      this.nome = nome;
+      this.sigla = sigla;
+      this.capital = capital;
+      this.habitantes = habitantes;
+    }
+  }
