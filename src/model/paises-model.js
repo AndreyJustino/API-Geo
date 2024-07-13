04dfae8e161d9e99e114b4a959326c6adb052c8e
AndreyJustino <andreyjustino2020@gmail.com>
2024-07-13 12:06:59 -0300

    aplicando model paises em dados

---
+++ src/model/paises-model.js
@@ -0,0 +1,9 @@
+export class Pais {
+    constructor(id, nome, presidente, habitantes, territorio){
+        this.id = id;
+        this.nome = nome;
+        this.presidente = presidente;
+        this.habitantes = habitantes;
+        this.territorio = territorio;
+    }
+}
